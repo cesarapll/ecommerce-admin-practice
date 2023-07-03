9c036c1a5f53c20782b7715cc00680aeb680bfc1
César Pizarro <102052430+cesarapll@users.noreply.github.com>
2023-07-03 03:00:06 -0500

    feat: Modal and store switcher

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -13,16 +13,7 @@ const SetupPage = () => {
     }
   }, [isOpen, onOpen]);
 
-  return (
-    <div className="p-4">
-      Root page
-      {/* <UserButton afterSignOutUrl="/" />
-
-      <Modal title="Test" description="Test Desc" isOpen onClose={() => {}}>
-        <h2>Children</h2>
-      </Modal> */}
-    </div>
-  );
+  return null;
 };
 
 export default SetupPage;
